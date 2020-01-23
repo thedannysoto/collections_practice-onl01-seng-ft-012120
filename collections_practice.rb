b6ee31d264f6417ba17cbb388d487a813f1d7a06
Danny Soto <87dsoto@gmail.com>
2020-01-23 17:23:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(num)
+  return num.sort
+end
 
+
+def sort_array_desc(num)
+  return num.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(array)
+  return array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  c = array[1]
+  b = array[2]
+  array [1] = b
+  array[2] = c
+  return array  
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$"
+    new_array << word
+  end
+  new_array
+end
